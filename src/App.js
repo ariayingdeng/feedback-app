16cd2d2840bae1c20118623dbc79a0c8b4cc86f4
Ying Deng <dengy9@student.douglascollege.ca>
2022-04-29 18:28:30 -0700

    Installed uuid for unique id and added a new feature to submit the form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
 import FeedbackData from './data/FeedbackData'
 import { useState } from 'react'
+import { v4 as uuidv4} from 'uuid'
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
@@ -13,11 +14,18 @@ function App() {
     }
   }
 
+  const addNewFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    if (window.confirm('Are you sure to submit your review?')) {
+      setFeedback([newFeedback, ...feedback]) //...anArray: copy all objects from anArray
+    }
+  }
+
   return (
     <>
       <Header text='Feedback App' />
       <div className='container'>
-        <FeedbackForm />
+        <FeedbackForm addFeedback={addNewFeedback}/>
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,23 +4,23 @@ import Button from './shared-components/Button'
 import RatingSelect from './RatingSelect'
 import { useState } from 'react'
 
-function FeedbackForm() {
+function FeedbackForm({ addFeedback }) {
   const [text, setText] = useState('')
   const [rating, setRating] = useState(10)
   const [btnDisabled, setBtnDisabled] = useState(true)
   const [message, setMessage] = useState('')
 
   const handleTextChange = (e) => {
-      if (text === '') {
-          setBtnDisabled(true)
-          setMessage(null)
-      } else if (text !== '' && text.trim().length <= 10) {
-        setBtnDisabled(true)
-        setMessage('Please enter at least 10 characters.')
-      } else {
-        setBtnDisabled(false)
-        setMessage(null)
-      }
+    if (text === '') {
+      setBtnDisabled(true)
+      setMessage(null)
+    } else if (text !== '' && text.trim().length <= 10) {
+      setBtnDisabled(true)
+      setMessage('Please enter at least 10 characters.')
+    } else {
+      setBtnDisabled(false)
+      setMessage(null)
+    }
     setText(e.target.value)
   }
 
@@ -29,11 +29,19 @@ function FeedbackForm() {
     setRating(selected)
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (text.trim().length > 10) {
+      const newFeedback = { text, rating }
+      addFeedback(newFeedback)
+    }
+  }
+
   return (
     <Card reverse={false}>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>How would you rate your experience with us?</h2>
-        <RatingSelect select={handleSelectChange}/>
+        <RatingSelect select={handleSelectChange} />
         <div className='input-group'>
           <input
             onChange={handleTextChange}
